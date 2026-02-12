5334cf3b4ffa89afbaed566f13c28a46ff631de9
Steveies <184891406+Stremiouser666@users.noreply.github.com>
2026-02-12 21:35:36 +0000

    Update MainActivity.kt

--- app/src/main/java/com/mookplayer/com/MainActivity.kt
+++ app/src/main/java/com/mookplayer/com/MainActivity.kt
@@ -1,43 +1,19 @@
-package com.mookplayer.com
+<?xml version="1.0" encoding="utf-8"?>
+<FrameLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent">
 
-import android.net.Uri
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.media3.common.MediaItem
-import androidx.media3.exoplayer.ExoPlayer
-import com.mookplayer.com.databinding.ActivityMainBinding
+    <androidx.media3.ui.PlayerView
+        android:id="@+id/playerView"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent" />
 
-class MainActivity : AppCompatActivity() {
+    <Button
+        android:id="@+id/selectFileButton"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="Select File"
+        android:layout_margin="16dp"
+        android:layout_gravity="top|end" />
 
-    private lateinit var binding: ActivityMainBinding
-    private var player: ExoPlayer? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        initializePlayer()
-    }
-
-    private fun initializePlayer() {
-        player = ExoPlayer.Builder(this).build()
-        binding.playerView.player = player
-
-        // Sample media for testing
-        val mediaItem = MediaItem.fromUri(
-            Uri.parse("https://storage.googleapis.com/exoplayer-test-media-0/play.mp3")
-        )
-
-        player?.setMediaItem(mediaItem)
-        player?.prepare()
-        player?.playWhenReady = true
-    }
-
-    override fun onStop() {
-        super.onStop()
-        player?.release()
-        player = null
-    }
-}
+</FrameLayout>
